ce3a3490d065998e4bef2ac3be7a5b5a7c4be932
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2022-08-31 23:40:18 +0300

    init commit

---
+++ FA-HelloWord/src/main/java/com/firstApp/FAHelloWord/FaHelloWordApplication.java
@@ -0,0 +1,14 @@
+package com.firstApp.FAHelloWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaHelloWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaHelloWordApplication.class, args);
+
+	}
+
+}
---
+++ FA-HelloWord/src/test/java/com/firstApp/FAHelloWord/FaHelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstApp.FAHelloWord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaHelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
