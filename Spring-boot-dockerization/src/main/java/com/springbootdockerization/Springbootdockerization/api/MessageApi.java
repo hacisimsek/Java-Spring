cae3eb8cf27cac240d19b5b3ef5153049f8ab09a
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2022-09-30 12:25:58 +0300

    init commit

---
+++ Spring-boot-dockerization/src/main/java/com/springbootdockerization/Springbootdockerization/api/MessageApi.java
@@ -0,0 +1,14 @@
+package com.springbootdockerization.Springbootdockerization.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageApi {
+    @GetMapping
+    public String getMessage(){
+        return "Hello from Docker Image";
+    }
+}
