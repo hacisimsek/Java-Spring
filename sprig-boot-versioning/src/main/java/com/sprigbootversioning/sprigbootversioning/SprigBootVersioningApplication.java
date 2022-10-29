0f3ee1b94c39dbe798c57c59505b991cb9201257
Haci Simsek <haci.simsek@sisal.com>
2022-10-29 10:27:08 +0300

    spring-boot-versioning app done

---
+++ sprig-boot-versioning/src/main/java/com/sprigbootversioning/sprigbootversioning/SprigBootVersioningApplication.java
@@ -0,0 +1,13 @@
+package com.sprigbootversioning.sprigbootversioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigBootVersioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigBootVersioningApplication.class, args);
+	}
+
+}
---
+++ sprig-boot-versioning/src/test/java/com/sprigbootversioning/sprigbootversioning/SprigBootVersioningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprigbootversioning.sprigbootversioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigBootVersioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
